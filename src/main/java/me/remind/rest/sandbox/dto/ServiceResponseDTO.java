cdfbbc511702b411f1dcb0520f0179500e427cae
Seungri Hwang <victoryhwang426@gmail.com>
2019-08-18 13:01:08 +0200

    Add controller test cases

--- src/main/java/me/remind/rest/sandbox/dto/ServiceResponseDTO.java
+++ src/main/java/me/remind/rest/sandbox/dto/ServiceResponseDTO.java
@@ -1,14 +1,25 @@
 package me.remind.rest.sandbox.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
 public class ServiceResponseDTO {
-
     private int userId;
     private int id;
     private String title;
     private String body;
+
+    @Builder
+    public ServiceResponseDTO(int userId,
+                              int id,
+                           String title,
+                           String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
---
+++ src/test/java/me/remind/rest/sandbox/controller/TestServiceControllerTest.java
@@ -0,0 +1,53 @@
+package me.remind.rest.sandbox.controller;
+
+import me.remind.rest.sandbox.dto.ServiceResponseDTO;
+import me.remind.rest.sandbox.service.TestService;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TestServiceController.class)
+public class TestServiceControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private TestService testService;
+
+    private ServiceResponseDTO serviceResponseDTO = ServiceResponseDTO.builder()
+            .id(1)
+            .userId(90000)
+            .body("Body")
+            .title("Title")
+            .build();
+
+    @Test
+    public void getServiceResponse() throws Exception {
+        when(testService.getTestServiceResult()).thenReturn(serviceResponseDTO);
+
+        mvc.perform(MockMvcRequestBuilders.get("/externalservice/").accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.jsonPath(
+                        "id",
+                        Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath(
+                        "userId",
+                        Matchers.is(90000)));
+    }
+
+}
